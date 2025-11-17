94db3d383b9b16230aff7d8a6d5b0713983c5365
Manirohith08 <135613002+Manirohith08@users.noreply.github.com>
2025-11-17 19:29:51 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,8 @@ import passport from "passport";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(
   cors({
     origin: [
@@ -16,22 +18,14 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" })); // to parse json in body
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to parse url
-app.use(express.static("public")); // to use static public folder
-app.use(cookieParser()); // to enable CRUD operation on browser cookies
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  // Add other CORS headers as needed
-  next();
-});
-
-// Passport middleware
 app.use(passport.initialize());
 
-// Importing routes
+// Routes
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import chatRouter from "./routes/chat.routes.js";
@@ -40,7 +34,6 @@ import requestRouter from "./routes/request.routes.js";
 import reportRouter from "./routes/report.routes.js";
 import ratingRouter from "./routes/rating.routes.js";
 
-// Using routes
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 app.use("/chat", chatRouter);
