176733c9f937a3b39da96e293f14127f34a5740c
Manirohith08 <135613002+Manirohith08@users.noreply.github.com>
2025-11-17 19:25:19 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://skill-barter-two.vercel.app",
+    ],
+    methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
